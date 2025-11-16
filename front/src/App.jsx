8f9f936e5e4bde2d803775c22a367f822add1999
Kenta Igarashi <igr4sy@gmail.com>
2025-11-16 17:18:02 +0900

    server setup

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState();
+
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.text())
+      .then((data) => {
+        console.log(data);
+
+        setMessage(data);
+      });
+  }, []);
 
   return (
     <>
@@ -28,8 +39,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>Message from the backend: {message}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
