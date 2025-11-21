9735fe513d825cb9e1d0639e57ba6aefad32055c
Kenta Igarashi <igr4sy@gmail.com>
2025-11-21 12:57:37 +0900

    ルーティング修正

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ function buildApp() {
 
   app.use('/', express.static(path.join(__dirname, '..', '/public')));
   app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
   });
 
   return app;
